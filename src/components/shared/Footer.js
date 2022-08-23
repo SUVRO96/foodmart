af5c39e8a02d01fb5163767fa993ff05bde246d3
Suvro96 <uniquesuvro@gmail.com>
2022-08-23 22:23:59 +0530

    added image

--- src/components/shared/Footer.js
+++ src/components/shared/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import footerBanner from "../assets/images/footerBanner.jpg";
+import footerBanner from "../assets/images/footBanner.jfif";
 
 const Footer = () => {
   return (
